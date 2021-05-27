7cbf5a11bc9c2d2647880f5e03a70e36b427e83e
Chamy <yd95069506@naver.com>
2021-05-27 10:14:55 +0900

    초기 커밋

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const port = 5000;
+
+app.use('/', (req,res) => {
+    res.send('hello world');
+});
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
