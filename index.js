4aa1a3d4beff3945ac01bf795981f703a3e2d63b
Chamy619 <yd95069506@naver.com>
2021-05-27 13:16:45 +0900

    body-parser 설치

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// Post 데이터를 받기 위한 body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// MongoDB 연동
 const mongoose = require('mongoose');
 const config = require('./config/key.js');
 mongoose.connect(config.mongoURI, {
