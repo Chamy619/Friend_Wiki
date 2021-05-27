766d7f501b78acfbe172b0c25cfb933fb287f245
Chamy619 <yd95069506@naver.com>
2021-05-27 13:12:27 +0900

    express - mongodb 연동

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const port = 5000;
+
+const mongoose = require('mongoose');
+const config = require('./config/key.js');
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.use('/', (req,res) => {
     res.send('hello world');
