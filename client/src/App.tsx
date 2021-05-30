975eb7c7f24fd5cb69738193540db872ed0a2f6f
Chamy619 <yd95069506@naver.com>
2021-05-30 14:41:40 +0900

    클라이언트(리액트 - 타입스크립트) 추가

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+    return (
+        <div>
+            랜딩페이지
+        </div>
+    );
+}
+
+export default LandingPage;
