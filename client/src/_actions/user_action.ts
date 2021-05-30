f4ab85e511917aa7354265e9f10860cc2a6058c4
Chamy619 <yd95069506@naver.com>
2021-05-30 20:03:48 +0900

    로그인  - 리덕스 사용해 구현

--- client/src/_actions/user_action.ts
+++ client/src/_actions/user_action.ts
@@ -1,18 +1,35 @@
 import axios from 'axios';
-import { REGISTER_USER } from './types';
+import {
+    REGISTER_USER,
+    LOGIN_USER
+} from './types';
 import { server } from './api_address';
 
-interface registerRequest {
+interface RegisterRequestInterface {
     email: string,
     password: string,
     name: string
 };
 
-export const registerUser = async (dataToSubmit: registerRequest) => {
-    const response = await axios.post(`${server}/api/user/register`, dataToSubmit)
+interface LoginRequestInterface {
+    email: string,
+    password: string
+};
+
+export const registerUser = async (dataToSubmit: RegisterRequestInterface) => {
+    const response = await axios.post(`${server}/api/user/register`, dataToSubmit);
 
     return ({
         type: REGISTER_USER,
+        payload: response.data
+    });
+}
+
+export const loginUser = async (dataToSubmit: LoginRequestInterface) => {
+    const response = await axios.post(`${server}/api/user/login`, dataToSubmit);
+
+    return ({
+        type: LOGIN_USER,
         payload: response.data
     });
 }
--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
-const Login: React.FC = (props) => {
+interface StyleInterface {
+    display: string,
+    justifyContent: string,
+    alignItems: string,
+    width: string,
+    height: string
+};
+
+const loginStyle: StyleInterface = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100vh'
+};
+
+const formStyle = {
+    display: 'flex',
+    flexDirection: 'column' as 'column'
+};
+
+const Login: React.FC = (props: any) => {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const onEmailHandler = (event: any) => {
+        setEmail(event.target.value);
+    };
+
+    const onPasswordHandler = (event: any) => {
+        setPassword(event.target.value);
+    }
+
+    const onSubmitHandler = async (event: any) => {
+        event.preventDefault();
+
+        const body = {
+            email,
+            password
+        };
+
+        const response = await dispatch(loginUser(body));
+
+        if (!response.payload.success) {
+            alert(response.payload.message);
+        } else {
+            localStorage.setItem('userToken', response.payload.token);
+            props.history.push('/');
+        }
+    }
+
     return (
-        <div>
-            로그인페이지
+        <div style={loginStyle}>
+            <form style={formStyle} onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={onPasswordHandler} />
+                <br />
+                <button>로그인</button>
+            </form>
         </div>
     );
 };
 
-export default Login;
+export default withRouter(Login);
