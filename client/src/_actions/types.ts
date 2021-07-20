fe9f9c68fbf248bee79c14d477907dba74c64a1e
Chamy619 <yd95069506@naver.com>
2021-07-20 22:19:37 +0900

    :wrench: gitignor 및 기존 파일 제거

--- client/src/_actions/types.ts
+++
@@ -1,4 +0,0 @@
-export const REGISTER_USER: string = 'register_user';
-export const LOGIN_USER: string = 'login_user';
-export const LOGOUT_USER: string = 'logout_user';
-export const AUTH_USER: string = 'auth_user';
