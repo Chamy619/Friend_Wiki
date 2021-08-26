838338735f7a971048f39a805f7bd15f02e05ec7
Chamy619 <yd95069506@naver.com>
2021-08-26 12:25:26 +0900

    :sparkles: PostViwer 컴포넌트 생성  - 제목, 내용, 글 작성자 및 작성 시간 확인 가능

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+import PostViewer from '../post/PostViewer';
+
+const HomeBlock = styled.div`
+  width: 100%;
+  padding: 0 2rem;
+`;
+
 function Home() {
-  return <div>홈 페이지</div>;
+  return (
+    <HomeBlock>
+      <PostViewer />
+    </HomeBlock>
+  );
 }
 
 export default Home;
